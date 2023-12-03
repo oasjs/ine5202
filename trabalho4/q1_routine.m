9dcf0d239c9cdebe599147972aad5353699a4a34
oasjs <oasjatoba1@gmail.com>
2023-12-02 21:40:32 -0300

    Add base scaffold

---
+++ trabalho4/q1_routine.m
@@ -0,0 +1,20 @@
+% 1a. (Opcional) Monte as 6 equações que determinam os m=3 coeficientes pesos C
+% e os m=3 pontos/nós t necessários no Método de Gauss-Legendre;
+
+% 1b. Determine os m=3 coeficientes c e m=3 pontos/nós t necessários no Método
+% de Gauss-Legendre através das 2*3 equações não lineares obtidas no item (1a),
+% use o código do Método de Newton genérico para n equações não lineares
+% (sugestões: xi = C's devem estar entre 0 e 1, pois a soma é 2; e t's devem
+% estar entre -1 e +1. Usar sub-relaxação.);
+
+% 1c. Alternativamente, determine os m=3 pontos/nós t necessários no Método de
+% Gauss-Legendre através das raízes do polinômio de Legendre de grau m=3;
+
+% 1d. Obtidos os m=3 pontos/nós t acima, determine os m=3 coeficientes pesos C
+% do Método de Gauss-Legendre através das 3 primeiras equações, linearizadas
+% com os 3 t´s obtidas no item 1c;
+
+function q1_routine()
+
+endfunction
+
